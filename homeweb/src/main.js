036b9d61a18702d4eb580e1bbd233c5a059908d7
wintelsui <wintelsui@hotmail.com>
2021-12-07 10:14:48 +0800

    init2
    init2

--- homeweb/src/main.js
+++ homeweb/src/main.js
@@ -1,12 +1,15 @@
-import Vue from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ArcoVue from '@arco-design/web-vue'; // https://arco.design/vue/docs/start
+import '@arco-design/web-vue/dist/arco.css';
 
-Vue.config.productionTip = false
-
-new Vue({
-  router,
-  store,
-  render: h => h(App)
-}).$mount('#app')
+const app = createApp(App)
+app.use(store)
+app.use(router)
+app.use(ArcoVue, {
+    // 用于改变使用组件时的前缀名称
+    componentPrefix: 'arco'
+})
+app.mount('#app')
