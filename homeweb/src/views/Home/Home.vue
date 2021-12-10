0fc70957c9b48fdbc9ebf1ff36552dab865c9ab9
wintelsui <wintelsui@hotmail.com>
2021-12-10 19:15:41 +0800

    home
    home

--- homeweb/src/views/Home/Home.vue
+++ homeweb/src/views/Home/Home.vue
@@ -1,17 +1,27 @@
+<!--
+ * @Author: your name
+ * @Date: 2021-12-05 22:55:23
+ * @LastEditTime: 2021-12-10 19:13:29
+ * @LastEditors: wintelsui
+ * @Description: 
+ * @FilePath: /homeweb/src/views/Home/Home.vue
+-->
 <template>
   <div class="home">
-
+    <div class="base-view">
+      <div class="content-view">
+        <div class="top-view-background-image">
+             <h1 style="color:#FFF">Welcome to wintelsui's home page!</h1>
+             <span class="byline" style="color:#FFF">let hello = ["hello","你好","Bonjour","Hola","Buon Giorno","こんにちは"]<br>let description = "我是一个喜欢技术的iOS开发者，喜欢分享，喜欢技术交流学习"</span>
+             <br><a href="http://github.com/wintelsui" style="color:#FFF">Github Repo</a>
+        </div>  
+      </div>
+    </div>
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-// import HelloWorld from '@/components/HelloWorld.vue'
+<script src="./_Home.js" lang="js"></script>
 
-export default {
-  name: 'Home',
-  components: {
-    // HelloWorld
-  }
-}
-</script>
+<style lang="scss" scoped>
+  @import "./_Home.scss";
+</style>
---
+++ homeweb/src/views/Home/_Home.js
@@ -0,0 +1,30 @@
+/*
+ * @Author: your name
+ * @Date: 2021-12-07 10:21:56
+ * @LastEditTime: 2021-12-10 17:37:41
+ * @LastEditors: your name
+ * @Description: 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ * @FilePath: /homeweb/src/views/Home/_Home.js
+ */
+// @vue/component
+export default {
+    name: 'Home',
+
+    components: {},
+
+    mixins: [],
+
+    props: {},
+
+    data () {
+        return {}
+    },
+
+    computed: {},
+
+    watch: {},
+
+    created () {},
+
+    methods: {}
+}
